a5ee173557d128af922d45bb52e99e6fd5b96f5e
jeremite <543084991@qq.com>
2020-10-19 22:18:56 -0700

    add new crimes functionality

--- dbhelper.py
+++ dbhelper.py
@@ -1,16 +1,16 @@
 import pymysql
 import dbconfig
-
+import datetime
 
 class DBHelper:
 
-  def connect(self, database="crimemap"):
-    return pymysql.connect(host='localhost',
-              user=dbconfig.db_user,
-              passwd=dbconfig.db_password,
-              db=database)
+    def connect(self, database="crimemap"):
+        return pymysql.connect(host='localhost',
+                  user=dbconfig.db_user,
+                  passwd=dbconfig.db_password,
+                  db=database)
 
-  def get_all_inputs(self):
+    def get_all_inputs(self):
         connection = self.connect()
         try:
           query = "SELECT description FROM crimes;"
@@ -20,18 +20,18 @@ class DBHelper:
         finally:
           connection.close()
 
-  def add_input(self, data):
+    def add_input(self, data):
         connection = self.connect()
         try:
           query = "INSERT INTO crimes (description) VALUES (%s);"
           with connection.cursor() as cursor:
-              cursor.execute(query, data)
+              cursor.execute(query, data) #execute can take data here as the string into %s
               connection.commit()
         finally:
               connection.close()
 
 
-  def clear_all(self):
+    def clear_all(self):
         connection = self.connect()
         try:
           query = "DELETE FROM crimes;"
@@ -39,4 +39,37 @@ class DBHelper:
             cursor.execute(query)
             connection.commit()
         finally:
+          connection.close()
+
+    def add_crime(self, category, date, latitude, longitude, description):
+      connection = self.connect()
+      try:
+        query = "INSERT INTO crimes (category, date, latitude, longitude, description) \
+          VALUES (%s, %s, %s, %s, %s)"
+        with connection.cursor() as cursor:
+          cursor.execute(query, (category, date, latitude, longitude, description))
+          connection.commit()
+      except Exception as e:
+        print(e)
+      finally:
+        connection.close()
+
+    def get_all_crimes(self):
+         connection = self.connect()
+         try:
+          query = "SELECT latitude, longitude, date, category, description FROM crimes;"
+          with connection.cursor() as cursor:
+           cursor.execute(query) #cursoe now is a tuple of tuples.
+          named_crimes = []
+          for crime in cursor:
+           named_crime = {
+            'latitude': crime[0],
+            'longitude': crime[1],
+            'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+            'category': crime[3],
+            'description': crime[4]
+           }
+           named_crimes.append(named_crime)
+          return named_crimes
+         finally:
           connection.close()
